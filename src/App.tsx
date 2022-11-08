0c9fe7b988c343f7b398cad9f0e3ef7adb752f45
Egorych <mikhalevichegor@gmail.com>
2022-11-09 01:38:15 +0300

    New component Cars.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-    import {Site} from "./HW_monday/site/SIte";
+import {Site} from "./HW_monday/site/SIte";
 import {StudentsArr} from "./HW_monday/students/StudentsArr";
+import {Cars} from "./HW_monday/cars/Cars";
 
 function App() {
   return (
     <div className="App">
     {/*<Site/>*/}
-      <StudentsArr/>
+    {/*  <StudentsArr/>*/}
+        <Cars/>
     </div>
   );
 }
