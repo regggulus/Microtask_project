cd26a05d8878c7d3c90ba36302cfffe63b860a73
Egorych <mikhalevichegor@gmail.com>
2022-11-08 23:34:46 +0300

    removed content and added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Body } from './site/Body';
+import {Header} from "./site/Header";
+import {Footer} from "./site/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header titleHead={"New Head"}/>
+      <Body titleBod={"New Body"}/>
+      <Footer titleFoot={"New Footer"}/>
+
     </div>
   );
 }
