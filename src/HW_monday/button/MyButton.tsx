603dc2344c9532208406b069004e5e40a0070258
Egorych <mikhalevichegor@gmail.com>
2022-11-10 00:10:31 +0300

    Разогрев с примерами

--- src/HW_monday/button/MyButton.tsx
+++ src/HW_monday/button/MyButton.tsx
@@ -2,12 +2,24 @@ import React, {MouseEvent} from "react";
 
 
 export function MyButton() {
-    const buttonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    /*const buttonClick = (event: MouseEvent<HTMLButtonElement>) => {
         console.log("Hi")
+    }*/
+    const onClickHandler = (name: string) => {
+        console.log(name)
+    }
+    const foo1 = (event: MouseEvent<HTMLButtonElement>) => {
+        console.log("Hi, foo1!!!")
+    }
+    const foo2 = (age: number) => {
+        console.log(age)
     }
     return (
         <div>
-            <button onClick={buttonClick}>Click Me</button>
+            <button onClick={(event) => onClickHandler("Bob")}>Click Me</button>
+            <button onClick={(event) => onClickHandler("Sam")}>Click Me</button>
+            <button onClick={foo1}>1</button>
+            <button onClick={ (event) => {foo2(100200)}}>2</button>
         </div>
     )
 }
