5270103922a58bfc8914670641d28c6665c20b8e
Egorych <mikhalevichegor@gmail.com>
2022-11-18 23:18:48 +0300

    callBack HW

--- src/HW_monday/filter/methodFilter.tsx
+++ src/HW_monday/filter/methodFilter.tsx
@@ -1,58 +1,38 @@
 import React, {useState} from "react";
+import {NewComponets} from "./NewComponets";
 
+export type MethodFilterType = {
+    banknots: string
+    value: number
+    number: string
+}
+export type FilterMoneyType = 'All' | 'RUBLS' | 'Dollars'
 export function MethodFilter() {
     const [money, setMoney] = useState([
-        {id: 1, banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {id: 2, banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {id: 3, banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {id: 4, banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {id: 5, banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {id: 6, banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {id: 7, banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {id: 8, banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'}
     ])
-    let currentMoney = money
-    {
-        if (buttonMon === "RUBLS") {
-            currentMoney = money.filter((filMon) => filMon.banknots === "RUBLS")
-            if (currentMoney === "Dollars") {
-                currentMoney = money.filter((filMon) => filMon.banknots === "Dollars")
-            }
-        }
+    const [filterMoney, setFilterMoney] = useState<FilterMoneyType>('All')
+
+    let filMon = money
+    if(filterMoney === 'RUBLS') {
+        filMon = money.filter( (filterObj) => filterObj.banknots === 'RUBLS')
+    } if (filterMoney === 'Dollars') {
+        filMon = money.filter( (filterObj) => filterObj.banknots === 'Dollars')
     }
 
-
-    const onClickHandler = (buttonMon: string) => {
-
-        return (
-
-            <div>
-
-            </div>
-        )
+    const onClickHandler = (nameButt: FilterMoneyType) => {
+        setFilterMoney(nameButt)
     }
     return (
         <div>
-            <ul>
-                {currentMoney.map((objMoney, index) => {
-                    return (
-
-                        //можно в key использовать id так же
-                        <li key={index}>
-                            <span>{objMoney.banknots}</span>
-                            <span>{objMoney.value}</span>
-                            <span>{objMoney.number}</span>
-                        </li>
-                    )
-                })
-                }
-
-            </ul>
-            <div style={{padding: "10px 0 0 30px"}}>
-                <button onClick={() => onClickHandler("All")}>All</button>
-                <button onClick={() => onClickHandler("RUBLS")}>Rub</button>
-                <button onClick={() => onClickHandler("Dollars")}>Dollars</button>
-            </div>
+            <NewComponets filMon={filMon} onClickHandler={onClickHandler} />
         </div>
 
 
