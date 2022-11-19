c1355d61f4de8c29fd2d192fe2267d94f8a9626f
Egorych <mikhalevichegor@gmail.com>
2022-11-19 12:15:54 +0300

    new component InputButton.tsx

---
+++ src/HW_monday/06 _Input/InputButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const InputButton = () => {
+    return (
+        <div>
+            <div>
+                <input type="text"/>
+                <button>+</button>
+            </div>
+        </div>
+    );
+};
+
