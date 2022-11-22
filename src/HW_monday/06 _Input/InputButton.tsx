7e5685ab43547b9ff9916818b7bfb12d0204f202
Egorych <mikhalevichegor@gmail.com>
2022-11-22 14:11:09 +0300

    06 - Input InputButton.tsx

--- src/HW_monday/06 _Input/InputButton.tsx
+++ src/HW_monday/06 _Input/InputButton.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {ChangeEvent, MouseEvent, useState} from 'react';
+import {Input} from "./Input";
+import {Button} from "./Button";
 
-export const InputButton = () => {
+
+export type InputButtonType = {
+    addTask: (title: string)=>void
+}
+export const InputButton = (props: InputButtonType) => {
+    const[title, setTitle] = useState('')
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event. currentTarget.value)
+    }
+    const onClickButtonHandler = ( ) => {
+        props.addTask(title)
+        setTitle('')
+    }
+
     return (
         <div>
             <div>
-                <input type="text"/>
-                <button>+</button>
+
+                <input value={title} onChange={onChangeHandler} type="text"/>
+                <button onClick={onClickButtonHandler}>+</button>
             </div>
         </div>
     );
