ec106f79320dad57b76f8325418d74b8beaf2c6a
Egorych <mikhalevichegor@gmail.com>
2022-11-22 23:47:19 +0300

    06 - Input new components callBackClick

--- src/HW_monday/06 _Input/StateApp.tsx
+++ src/HW_monday/06 _Input/StateApp.tsx
@@ -1,28 +1,29 @@
 import React, {useState} from 'react';
-import {InputButton} from "./InputButton";
 import {Input} from "./Input";
 import {Button} from "./Button";
 
 export function StateApp() {
-    let [message, setMessage] = useState( [
+    let [message, setMessage] = useState([
         {message: 'message1'},
         {message: 'message2'},
         {message: 'message3'}
     ])
-    let [title, setTitle]=useState('')
+    let [title, setTitle] = useState('')
     const addTask = (title: string) => {
         let newTask = {message: title}
         setMessage([newTask, ...message])
-
     }
-
+    const callBackClick = () => {
+        addTask(title)
+        setTitle('')
+    }
     return (
         <div>
             {/*<InputButton addTask={addTask}/>*/}
             <Input title={title} setTitle={setTitle}/>
-            <Button name={"+"} callBack={ ()=>{}}/>
+            <Button name={"+"} callBack={callBackClick}/>
 
-            {message.map( (m, index) => {
+            {message.map((m, index) => {
                 return (
                     <ul>
                         <li key={index}>{m.message}</li>
