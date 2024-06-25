d8055bf73ead5eaa592ee06de6e66f2e81b8b1a9
Egorych <mikhalevichegor@gmail.com>
2024-06-26 00:30:28 +0300

    table Counter.tsx

--- src/Sprint_1/useStateLesson/Counter.tsx
+++ src/Sprint_1/useStateLesson/Counter.tsx
@@ -1,18 +1,29 @@
 import {useState} from "react";
 
 export const Counter = () => {
-   let [count, setCount] = useState(0)
+    let [count, setCount] = useState(0)
     const onClickCountHandler = () => {
-       setCount(++count)
+        setCount(++count)
     }
     const onClickSetHandler = () => {
-       setCount(0)
+        setCount(0)
     }
     return (
-        <div>
-            <h3>{count}</h3>
-            <button onClick={onClickCountHandler}>count</button>
-            <button onClick={onClickSetHandler}>set</button>
-        </div>
+        <table style={{border: '1px solid black',}}>
+                <tr >
+                <th style={{border: '1px solid black', display: 'flex', justifyContent: 'center', textAlign: 'center'}}><h3>{count}</h3>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <button onClick={onClickCountHandler}>count</button>
+                </td>
+                <td>
+                    <button onClick={onClickSetHandler}>set</button>
+
+                </td>
+            </tr>
+
+        </table>
     )
 }
