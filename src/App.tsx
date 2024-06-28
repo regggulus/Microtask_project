8e89e54d6aa2224f466a60de1148cf8d4efe6169
Egorych <mikhalevichegor@gmail.com>
2024-06-28 23:55:35 +0300

    method filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {NestedComponents} from "./Sprint_1/nestedComponents/NestedComponents";
 import {DateState} from "./Sprint_1/mapMethod/DateState";
 import {ButtonUsers} from "./Sprint_1/buttonUniversal/ButtonUsers";
 import {Counter} from "./Sprint_1/useStateLesson/Counter";
-import {FilterMethod} from "./Sprint_1/filteredMethod/FilterMethod";
 
 
 function App() {
+    const [money, setMoney] = useState([
+        {banknotes: 'Dollars -', nominal: 100 , number: ' a1234567890'},
+        {banknotes: 'Dollars -', nominal: 50, number: ' z1234567890'},
+        {banknotes: 'Rubles -', nominal: 100, number: ' w1234567890'},
+        {banknotes: 'Dollars -', nominal: 100, number: ' e1234567890'},
+        {banknotes: 'Dollars -', nominal: 50, number: ' c1234567890'},
+        {banknotes: 'Rubles -', nominal: 100, number: ' r1234567890'},
+        {banknotes: 'Dollars -', nominal: 50, number: ' x1234567890'},
+        {banknotes: 'Rubles -', nominal: 50, number: ' v1234567890'},
+        {banknotes: 'Rubles -', nominal: 5000, number: ' v1234567890'},
+    ])
+    let currentMoney = money.filter(
+        (filterM)=> filterM.banknotes === 'Dollars')
     return (
         <div className="App">
             {/*<NestedComponents/>*/}
             {/*<DateState/>*/}
             {/*<ButtonUsers/>*/}
             {/*<Counter/>*/}
-            <FilterMethod/>
+
+            <ul>
+                {currentMoney.map((m, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{m.banknotes }</span>
+                            <span>{m.nominal   }</span>
+                            <span>{m.number}</span>
+                        </li>
+
+                    )
+                })}
+
+            </ul>
+
         </div>
     );
 }
