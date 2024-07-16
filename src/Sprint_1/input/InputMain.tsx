6653737e2b0ae292d3a55e531d130eb459b96126
Egorych <mikhalevichegor@gmail.com>
2024-07-17 00:19:30 +0300

    components InputMain.tsx

---
+++ src/Sprint_1/input/InputMain.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const InputMain = () => {
+    return (
+        <div>
+            <input type="text"/>
+        </div>
+    );
+};
--- src/Sprint_1/input/StateMessage.tsx
+++ src/Sprint_1/input/StateMessage.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {ButtonUsers} from "../buttonUniversal/ButtonUsers";
 import {ButtonUniversal} from "../buttonUniversal/ButtonUniversal";
+import {InputMain} from "./InputMain";
 
 export const StateMessage = () => {
     const [message, setMessage] = useState([
@@ -11,7 +12,7 @@ export const StateMessage = () => {
     ])
     return (
         <div>
-            <input/>
+            <InputMain/>
             <ButtonUniversal name={'+'} callback={ () => {}}/>
             <ul >
                 {message.map((m, index) => {
