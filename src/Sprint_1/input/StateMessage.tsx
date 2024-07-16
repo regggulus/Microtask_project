a0957a0884d9a2da962ce7725aa5646b9b645fc1
Egorych <mikhalevichegor@gmail.com>
2024-07-17 00:49:22 +0300

    function addMessage

--- src/Sprint_1/input/StateMessage.tsx
+++ src/Sprint_1/input/StateMessage.tsx
@@ -4,22 +4,23 @@ import {ButtonUniversal} from "../buttonUniversal/ButtonUniversal";
 import {InputMain} from "./InputMain";
 
 export const StateMessage = () => {
-    const [message, setMessage] = useState([
+    const [message, setMessages] = useState([
         {messages: 'message 1'},
         {messages: 'message 2'},
         {messages: 'message 3'},
         {messages: 'message 4'}
     ])
 
-    const AddMessage = (message: string) => {
-
+    const addMessage = () => {
+        console.log('yo')
     }
 
     return (
         <div>
-            <InputMain callback={}/>
-            <ButtonUniversal name={'+'} callback={ () => {}}/>
-            <ul >
+            <InputMain callback={addMessage}/>
+            <ButtonUniversal name={'+'} callback={() => {
+            }}/>
+            <ul>
                 {message.map((m, index) => {
                     return (
                         <li key={index}>{m.messages}
